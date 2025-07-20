c7734bfcc8a3f4761ec1cac0dbf529b4bd6d2aed
Sahil-Bhanushali <sahilbhanushali5678@gmail.com>
2025-07-20 10:49:33 +0530

    added the code to jobWorker from server

--- server/src/server.js
+++ server/src/server.js
@@ -5,12 +5,7 @@ import { connectDB } from "./config/db.js";
 import importLogsRoute from "./routes/importLogs.js";
 import jobsRoute from "./routes/jobs.js";
 import { fetchJobs } from "./jobs/jobsFetcher.js";
-
-// Worker-related imports
-import { Worker } from "bullmq";
-import redisConfig from "./config/redis.js";
-import Job from "./models/jobs.js";
-import ImportLog from "./models/importLogs.js";
+import { startWorker } from "./jobs/jobWorker.js";
 
 dotenv.config();
 const app = express();
@@ -28,79 +23,5 @@ app.listen(PORT, async () => {
   console.log(` Server is running on port ${PORT}`);
 
   await fetchJobs();
-
-  const worker = new Worker(
-    "job-importer",
-    async (job) => {
-      const jobs = job.data.jobs || [];
-
-      let newJobs = 0;
-      let updatedJobs = 0;
-      let failedJobs = [];
-
-      for (const j of jobs) {
-        try {
-          const jobId = j.link || j.id || j.guid;
-          if (!jobId) throw new Error("Missing job ID");
-
-          const jobData = {
-            jobId: jobId,
-            title: j.title,
-            link: j.link,
-            publishedAt: new Date(j.pubDate),
-            description: j.description,
-            company: j.company || "Unknown",
-            jobType: j.jobType || "Unknown",
-            jobLocation: j.jobLocation || "Remote",
-          };
-
-          const existing = await Job.findOne({ jobId });
-
-          if (existing) {
-            await Job.updateOne({ jobId }, jobData);
-            updatedJobs++;
-          } else {
-            await Job.create(jobData);
-            newJobs++;
-          }
-        } catch (err) {
-          failedJobs.push({ jobId: j.link || "N/A", reason: err.message });
-        }
-      }
-
-      await ImportLog.create({
-        totalFetched: jobs.length,
-        totalImported: newJobs + updatedJobs,
-        newJobs,
-        updatedJobs,
-        failedJobs,
-      });
-
-      return `Import Summary: ${newJobs} new, ${updatedJobs} updated, ${failedJobs.length} failed`;
-    },
-    {
-      connection: redisConfig,
-      lockDuration: 600000,
-      removeOnComplete: {
-        age: 3600,
-        count: 1000,
-      },
-      removeOnFail: {
-        age: 3600,
-        count: 500,
-      },
-    }
-  );
-
-  worker.on("active", (job) => {
-    console.log(` Worker started processing job ${job.id}`);
-  });
-
-  worker.on("completed", (job) => {
-    console.log(` Worker finished job ${job.id}`);
-  });
-
-  worker.on("failed", (job, err) => {
-    console.error(` Worker failed job ${job.id}:`, err.message);
-  });
+  startWorker();
 });
